2e30dfc1bd370bfdb9421f10f61db15d37a4632a
Niraj Kumar <n1r4jkumar@gmail.com>
2022-10-30 08:00:36 +0530

    change the directory

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,40 +1,11 @@
 import React, { useState } from "react";
+import UseState from "./UseState/UseState";
 
 function App() {
-    // const [count, setCount] = useState(4);
-    // const [count, setCount] = useState(() => {
-    //     console.log("Run state");
-    //     return 4;
-    // });
 
-    const [state, setState] = useState({ count: 4, theme: "blue" });
-    const count = state.count;
-    const theme = state.theme;
-
-
-    const decrementCount = () => {
-        // setCount(count - 1);
-        // setCount(preCount => preCount - 1);
-
-        setState(prevState => {
-            return { ...prevState, count: prevState.count - 1 }
-        })
-    }
-    const incrementCount = () => {
-        // setCount(preCount => preCount + 1); 
-        setState(prevState => {
-            return { ...prevState, count: prevState.count + 1 }
-        })
-    }
 
     return (
-        <>
-
-            <button onClick={decrementCount}>-</button>
-            <span>{count}</span>
-            <span>{theme}</span>
-            <button onClick={incrementCount}>+</button>
-        </>
+        <UseState />
     );
 }
 
