78503d5197386cfe676ac90f787ce4ef66f09ade
Niraj Kumar <n1r4jkumar@gmail.com>
2022-10-30 07:55:58 +0530

    useState added

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
 
 function App() {
+    // const [count, setCount] = useState(4);
+    // const [count, setCount] = useState(() => {
+    //     console.log("Run state");
+    //     return 4;
+    // });
 
-    const [count, setCount] = useState(4);
+    const [state, setState] = useState({ count: 4, theme: "blue" });
+    const count = state.count;
+    const theme = state.theme;
+
 
     const decrementCount = () => {
         // setCount(count - 1);
-        setCount(preCount => preCount - 1);
-        setCount(preCount => preCount - 1);
+        // setCount(preCount => preCount - 1);
+
+        setState(prevState => {
+            return { ...prevState, count: prevState.count - 1 }
+        })
     }
     const incrementCount = () => {
-        setCount(count + 1);
+        // setCount(preCount => preCount + 1); 
+        setState(prevState => {
+            return { ...prevState, count: prevState.count + 1 }
+        })
     }
 
     return (
@@ -18,6 +32,7 @@ function App() {
 
             <button onClick={decrementCount}>-</button>
             <span>{count}</span>
+            <span>{theme}</span>
             <button onClick={incrementCount}>+</button>
         </>
     );
